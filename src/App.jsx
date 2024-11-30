7513e1a06a0c22bebc4967857f2dc8cf21b3c52b
pangeacd <creditosdirectos@pangeatech.com.ve>
2024-11-30 13:41:55 -0400

    primer commit a desarrollo
    Signed-off-by: pedpab.12 <creditosdirectos@pangeatech.com.ve>

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+function App() {
+
+  /**
+   * @description función que suma numero
+   * @author pedro Parada (30-11-2023), brayham (01-01-2025)
+   * @param {int, int}  numero a sumar
+   * @returns retorna suma de dos números
+   */
+
+
+  return (
+    <>
+<h1> Plantilla Workflow</h1>   
+
+ </>
+  )
+}
+
+export default App
